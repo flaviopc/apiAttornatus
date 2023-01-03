c048a6af3a63d7d5e0aeca5fef88ce30cf923cc2
Flávio Paixão <flavio_pc@outlook.com>
2023-01-03 11:26:57 -0300

    commit inicial

---
+++ src/test/java/com/attornatus/proj/ProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
